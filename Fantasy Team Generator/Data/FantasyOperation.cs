c456803ac30dfbcf507d3931f0f7234623377c19
mayurk <mayurkachare8@gmail.com>
2024-04-05 22:40:31 +0530

    Get team data

---
+++ Fantasy Team Generator/Data/FantasyOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Team_Generator.Data
+{
+    public class FantasyOperation
+    {
+        public List<string> GetTeam()
+        {
+            List<string> team= new List<string>();
+            string playerName;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Enter {i+1} Name of player");
+                playerName= Console.ReadLine()!;
+                team.Add(playerName);
+            }
+
+            return team;
+        }
+
+        public List<string> GetFantasyTeam()
+        {
+            List<string> fantasyTeam = new List<string>();
+
+            return fantasyTeam;
+
+        }
+    }
+}
---
+++ Fantasy Team Generator/Startup.cs
@@ -0,0 +1,10 @@
+﻿using Fantasy_Team_Generator.Data;
+
+public class Startup
+{
+    public static void Main(string[] args) {
+        FantasyOperation fantasyOperation = new FantasyOperation();
+
+        fantasyOperation.GetTeam();
+    }
+}
